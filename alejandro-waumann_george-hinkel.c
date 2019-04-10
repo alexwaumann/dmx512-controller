019ec808d14e0f7496b9f77c771c3f225002bb17
George Hinkel <george.hinkel@gmail.com>
2019-04-10 17:09:30 -0500

    added UART1 ISR to handle loading the FIFOs for dmx data transmission

--- alejandro-waumann_george-hinkel.c
+++ alejandro-waumann_george-hinkel.c
@@ -7,18 +7,13 @@
 #define DMX_DE (*((volatile uint32_t *)(0x42000000 + (0x400063FC-0x40000000)*32 + 6*4)))
 
 int dmx512_state;
+int dmx512_max = 512;
+char dmx512_data[513];
 
 void init_hw( void );
 
 void init_uart_coms( uint32_t sys_clock, uint32_t baud_rate );
 void init_uart_dmx( void );
-
-int main(void)
-{
-    init_hw();
-
-    while( 1 );
-}
 
 void init_hw( void )
 {
@@ -48,13 +43,19 @@ void init_uart_coms( uint32_t sys_clock, uint32_t baud_rate )
                  |  UART_CTL_TXE;
 }
 
+void dmx_prime(void)
+{
+    //Break
+    GPIO_PORTC_AFSEL_R  &= ~32;                 // clear 6th bit to disable peripheral control for PC5
+    //TODO set the timer1A ILR to a value for 176 us
+    TIMER1_TAILR_R = 0;                         // set Timer1A ILR to appropriate value for 176 us
+    DMX_DE = 1;                                 // drive DMX_DE high to enable transmission
+    DMX_TX = 0;                                 // drive DMX_TX low
+    TIMER1_CTL_R |= TIMER_CTL_TAEN;             // turn on timer 1
+}
+
 void init_uart_dmx( void )
 {
-    //TODO
-    /*
-     * Set the sys_clock variable or replace in code with hard value
-     * set the timer1A ILR to a value for 176 us
-     */
     dmx512_state = 0;
 
     //GPIO PORTC :: for DMX-512
@@ -64,6 +65,7 @@ void init_uart_dmx( void )
     GPIO_PORTC_ODR_R    &= ~112;                // set PC5 and PC4 ODR to 0
 
     //UART1 :: for DMX-512 Transmission
+    //TODO set sysclock
     uint32_t sys_clock = 0;
     uint32_t baud_rate = 250000;
     float    brd  = (float) sys_clock / (16.0f * (float) baud_rate);
@@ -76,8 +78,11 @@ void init_uart_dmx( void )
     UART1_IBRD_R |= bri;                        // baud rate divisor integer part
     UART1_FBRD_R |= brf;                        // baud rate divisor fractional part
     UART1_LCRH_R |= UART_LCRH_WLEN_8            // 8-bit data frame
-                 | UART_LCRH_STP2;              // 2 stop-bits
+                 |  UART_LCRH_STP2              // 2 stop-bits
+                 |  UART_LCRH_FEN;              // enable transmit and receive FIFOs
     UART1_CC_R    = UART_CC_CS_SYSCLK;          // use system clock
+    UART1_IFLS_R |= UART_IFLS_TX1_8;            // set transmit interrupt to go off when transmit FIFO is 1/8th full
+    NVIC_EN0_R |= 1 << (INT_UART1 - 16);        // turn on interrupt 22 (UART1)
 
     //TIMER1 :: for DMX-512 Break and MAB
     SYSCTL_RCGCTIMER_R |= SYSCTL_RCGCTIMER_R1;  // turn on timer
@@ -88,31 +93,45 @@ void init_uart_dmx( void )
     NVIC_EN0_R |= 1 << (INT_TIMER1A - 16);      // turn on interrupt 37 (TIMER1A)
 
     //Break
-    GPIO_PORTC_AFSEL_R  &= ~32;                 // clear 6th bit to disable peripheral control for PC5
-    TIMER1_TAILR_R = 0;                         // set Timer1A ILR to appropriate value for 176 us
-    DMX_TX = 0;                                 // drive DMX_TX low
-    TIMER1_CTL_R |= TIMER_CTL_TAEN;             // turn on timer 1
+    dmx_prime();
+}
 
+void Uart1ISR(void){
+    for(int i=0;i<16;i++)
+    {
+        UART1_DR_R = dmx512_data[(dmx512_state++) - 2];
+        if((dmx512_state - 2) >= dmx512_max )
+        {
+            dmx_prime();
+            return;
+        }
+    }
 }
 
 void Timer1ISR(void){
     if(dmx512_state == 0)
     {
-        TIMER1_CTL_R &= ~TIMER_CTL_TAEN;            // turn off timer before configuring
-        TIMER1_TAILR_R = 0;                         // set Timer1A ILR to appropriate value for 12 us
+        //TODO set TAILR_R to value for 12 us
         dmx512_state = 1;                           // advance dmx512 state
+        TIMER1_TAILR_R = 0;                         // set Timer1A ILR to appropriate value for 12 us
         DMX_TX = 1;                                 // drive DMX_TX high
         TIMER1_CTL_R |= TIMER_CTL_TAEN;             // turn on timer 1
     }
     else if(dmx512_state == 1)
     {
-        TIMER1_CTL_R &= ~TIMER_CTL_TAEN;            // turn off timer
+        dmx512_state = 2;                           // advance dmx512 state
         GPIO_PORTC_AFSEL_R |= 32 | 16;              // set 5th and 6th bits to enable peripheral control for PC5 and PC4
         GPIO_PORTC_PCTL_R  |= GPIO_PCTL_PC5_U1TX    // UART1 TX ON PC5
                            |  GPIO_PCTL_PC4_U1RX;   // UART1 RX on PC4
         UART1_CTL_R  |= UART_CTL_UARTEN             // enable UART1
                      |  UART_CTL_TXE;               // enable UART1 transmission
-        dmx512_state = 2;                           // advance dmx512 state
+        UART1_DR_R = dmx512_data[(dmx512_state++) - 2];
     }
+}
 
+int main(void)
+{
+    init_hw();
+
+    while( 1 );
 }
--- tm4c123gh6pm_startup_ccs.c
+++ tm4c123gh6pm_startup_ccs.c
@@ -34,6 +34,7 @@ static void NmiSR(void);
 static void FaultISR(void);
 static void IntDefaultHandler(void);
 void Timer1ISR(void);
+void Uart1ISR(void);
 
 //*****************************************************************************
 //
@@ -90,7 +91,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // GPIO Port D
     IntDefaultHandler,                      // GPIO Port E
     IntDefaultHandler,                      // UART0 Rx and Tx
-    IntDefaultHandler,                      // UART1 Rx and Tx
+    Uart1ISR,                      // UART1 Rx and Tx
     IntDefaultHandler,                      // SSI0 Rx and Tx
     IntDefaultHandler,                      // I2C0 Master and Slave
     IntDefaultHandler,                      // PWM Fault
