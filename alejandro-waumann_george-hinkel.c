d53a672b7463941d0fac05fe622bc94f74e4a7fc
George Hinkel <george.hinkel@gmail.com>
2019-04-10 12:58:14 -0500

    added GPIO config stuff for UART1 and DMX512

--- alejandro-waumann_george-hinkel.c
+++ alejandro-waumann_george-hinkel.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 
 #define PC5_MASK 32
+#define DMX_TX (*((volatile uint32_t *)(0x42000000 + (0x400063FC-0x40000000)*32 + 5*4)))
 
 int dmx512_state;
 
@@ -54,8 +55,9 @@ void init_uart_dmx( void )
 
     SYSCTL_RCGCGPIO_R  |= SYSCTL_RCGCGPIO_R2;   // enable PORTC module
     SYSCTL_RCGCUART_R  |= SYSCTL_RCGCUART_R1;   // enable UART1 module
-    GPIO_PORTC_AFSEL_R |= 32;                   // set 6th bit to enable peripheral control for PC5
-    GPIO_PORTC_PCTL_R  |= GPIO_CPTL_PC5_U1TX;   // UART1 TX ON PC5
+    GPIO_PORTC_DIR_R    |= 32;                  // set PC5 direction as output
+    GPIO_PORTC_DEN_R    |= 32 | 16;             // set PC5/DMX_TX and PC4 as digital I/O
+    GPIO_PORTC_ODR_R    &= ~48;                 // set PC5 and PC4 ODR to 0
 
     //UART1 :: for DMX-512 Transmission
     float    brd  = (float) sys_clock / (16.0f * (float) baud_rate);
@@ -63,11 +65,11 @@ void init_uart_dmx( void )
     uint8_t  brf  = (int) ((brd - (float) bri) * 64.0f + 0.5f);
              brf &= 0x3F;                       // only need bottom 6 bits
 
-    UART1_CTL_R  &= ~UART_CTL_UARTEN;           // disable UART0 for configuration
+    UART1_CTL_R  &= ~UART_CTL_UARTEN;           // disable UART1 for configuration
     UART1_IBRD_R |= bri;                        // baud rate divisor integer part
     UART1_FBRD_R |= brf;                        // baud rate divisor fractional part
     UART1_LCRH_R |= UART_LCRH_WLEN_8            // 8-bit data frame
-                 | UART_LCRH_STP2;
+                 | UART_LCRH_STP2;              // 2 stop-bits
     UART1_CC_R    = UART_CC_CS_SYSCLK;          // use system clock
 
     //TIMER1 :: for DMX-512 Break and MAB
@@ -79,8 +81,10 @@ void init_uart_dmx( void )
     NVIC_EN0_R |= 1 << (INT_TIMER1A - 16);      //turn on interrupt 37 (TIMER1A)
 
     //Break
-    GPIO_PORTC_DATA_R ;
-    TIMER1_CTL_R |= TIMER_CTL_TAEN;             //turn on timer
+    GPIO_PORTC_AFSEL_R  &= ~32;                 // clear 6th bit to disable peripheral control for PC5
+
+    DMX_TX               = 0;                   // drive DMX_TX low
+    TIMER1_CTL_R        |= TIMER_CTL_TAEN;      //turn on timer
 
     // setup UART1 using ports PC4-5
     // setup interrupts
@@ -90,8 +94,10 @@ void Timer1ISR(void){
     if(dmx512_state == 0){
 
     }else if(dmx512_state == 1){
-
-        UART1_CTL_R  |= UART_CTL_UARTEN             // enable UART1 and TX
+        GPIO_PORTC_AFSEL_R |= 32 | 16;              // set 5th and 6th bits to enable peripheral control for PC5 and PC4
+        GPIO_PORTC_PCTL_R  |= GPIO_PCTL_PC5_U1TX    // UART1 TX ON PC5
+                           |  GPIO_PCTL_PC4_U1RX;   // UART1 RX on PC4
+        UART1_CTL_R  |= UART_CTL_UARTEN             // enable UART1 and Transmission
                      |  UART_CTL_TXE;
     }
 
