4edb796d0ecf2dad12b01dfba8745b3f39067cb0
Alejandro Waumann <awaumann@gmail.com>
2019-04-16 15:37:12 -0500

    initialized the sysclk

--- alejandro-waumann_george-hinkel.c
+++ alejandro-waumann_george-hinkel.c
@@ -19,6 +19,15 @@ void init_uart_dmx( void );
 
 void init_hw( void )
 {
+	// Configure HW to work with 16 MHz XTAL, PLL enabled, system clock of 40 MHz
+    SYSCTL_RCC_R = SYSCTL_RCC_XTAL_16MHZ | SYSCTL_RCC_OSCSRC_MAIN | SYSCTL_RCC_USESYSDIV | (4 << SYSCTL_RCC_SYSDIV_S);
+
+    // Set GPIO ports to use APB (not needed since default configuration -- for clarity)
+    // Note UART on port A must use APB
+    SYSCTL_GPIOHBCTL_R = 0;
+
+    // Enable GPIO port A and F peripherals
+    SYSCTL_RCGC2_R = SYSCTL_RCGC2_GPIOA | SYSCTL_RCGC2_GPIOF;
 }
 
 void init_uart_coms( uint32_t sys_clock, uint32_t baud_rate )
